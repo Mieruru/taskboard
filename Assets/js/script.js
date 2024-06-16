cc75357e402936474af9d93ca0cbfb6e9cc3b904
Cassidy Pagels <rozeneclipse@gmail.com>
2024-06-16 05:06:03 -0500

    generate unique task id function, create modal form popup

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,9 +2,16 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
+const modal = $("#formModal")
+
+
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+    let currentId = nextId
+    nextId += 1
+    localStorage.setItem("nextId", nextId)
+    localStorage.clear()
+    return currentId
 }
 
 // Todo: create a function to create a task card
@@ -19,7 +26,8 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
-
+    console.log('in add task event')
+    console.log(generateTaskId())
 }
 
 // Todo: create a function to handle deleting a task
@@ -34,5 +42,20 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+    if (nextId === null) {
+        generateTaskId()
+    }
+    
+    //render task list
+    renderTaskList()
+    //add event listeners
+    $("#task-form-button").on('click', function() {
+        handleAddTask()
+    })
 
+    //make lanes droppable
+
+    //make due date field a date picker
+    $( "#datepicker" ).datepicker()
+    console.log('hello, world')
 });
