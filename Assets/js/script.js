a6afb0af9227590ce2f8d8fb292ce7fe5cfa7ffd
Cassidy Pagels <rozeneclipse@gmail.com>
2024-10-24 17:16:00 -0500

    script.js format scrub

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,11 +7,11 @@ const modal = $("#formModal")
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-    let currentId = nextId
-    nextId += 1
-    localStorage.setItem("nextId", nextId)
-    localStorage.clear()
-    return currentId
+  let currentId = nextId
+  nextId += 1
+  localStorage.setItem("nextId", nextId)
+  localStorage.clear()
+  return currentId
 }
 
 // Todo: create a function to create a task card
@@ -25,13 +25,13 @@ function renderTaskList() {
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
-    console.log('in add task event')
-    console.log(generateTaskId())
+function handleAddTask(event) {
+  console.log('in add task event')
+  console.log(generateTaskId())
 }
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
+function handleDeleteTask(event) {
 
 }
 
@@ -42,20 +42,20 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-    if (nextId === null) {
-        generateTaskId()
-    }
-    
-    //render task list
-    renderTaskList()
-    //add event listeners
-    $("#task-form-button").on('click', function() {
-        handleAddTask()
-    })
+  if (nextId === null) {
+    generateTaskId()
+  }
 
-    //make lanes droppable
+  //render task list
+  renderTaskList()
+  //add event listeners
+  $("#task-form-button").on('click', function () {
+    handleAddTask()
+  })
 
-    //make due date field a date picker
-    $( "#datepicker" ).datepicker()
-    console.log('hello, world')
+  //make lanes droppable
+
+  //make due date field a date picker
+  $("#datepicker").datepicker()
+  console.log('hello, world')
 });
